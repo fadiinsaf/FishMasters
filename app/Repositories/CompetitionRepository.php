668b5c0e86a49396de432c41ef5c1f5a16a53a42
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-01-21 19:39:22 +0100

    revert: improve return type of methods and implement getByCategory method

--- app/Repositories/CompetitionRepository.php
+++ app/Repositories/CompetitionRepository.php
@@ -1,23 +1,34 @@
 <?php
+
 namespace App\Repositories;
+
 use App\Models\Competition;
 use config\Database;
 use PDO;
 
 class CompetitionRepository
 {
-    public function getAll() : array
+    public function getAll(): array
     {
         $pdo = Database::getInstance();
         $sql = 'SELECT * FROM competition';
         $stmt = $pdo->prepare($sql);
-        return $stmt->fetchAll(PDO::FETCH_OBJ );
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
     }
-    public function getById(int $id) : ?Competition {
+
+    public function getById(int $id): ?array
+    {
         $pdo = Database::getInstance()->getConnection();
         $sql = 'SELECT * FROM competition WHERE id_competition = :id';
         $stmt = $pdo->prepare($sql);
-        return $stmt->execute(['id' => $id])  ? $stmt->fetch(PDO::FETCH_OBJ) : null;
+        return $stmt->execute([':id' => $id]) ? $stmt->fetch(PDO::FETCH_OBJ) : null;
     }
-    public function getByCategorie(int $idCategorie) : ?Competition {}
+
+    public function getByCategorie(int $idCategorie): ?array
+    {
+        $pdo = Database::getInstance()->getConnection();
+        $sql = 'SELECT * FROM competition WHERE id_categorie = :idCategorie';
+        $stmt = $pdo->prepare($sql);
+        return $stmt->execute([':idCategorie' => $idCategorie]) ? $stmt->fetch(PDO::FETCH_OBJ) : null;
+    }
 }
