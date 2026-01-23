d4275532606c5e10360587b25736c32ab61b38ed
mohamed-elhamdaoui <mohamedelhamdaoui007@gmail.com>
2026-01-23 12:31:27 +0100

    chore(config): update database singleton and settings

--- config/Config.php
+++ config/Config.php
@@ -1,10 +1,6 @@
 <?php
-<<<<<<< HEAD
-define('DB_TYPE', 'pgsql');
-=======
 namespace config;
 define('DB_TYPE', 'psql');
->>>>>>> c9b7a07888860c409b6aca5fac735bf6b91b0675
 define('DB_HOST', 'www.dockhosting.com');
 define('DB_USER', 'admin');
 define('DB_PASS', '9d1ca7f0d24698690cd701d1');
