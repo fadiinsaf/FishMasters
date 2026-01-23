0819cf106f59b509fdeb99f05c7ebe1d6d976ade
Mariam982440 <mariam1elhaimer@gmail.com>
2026-01-23 12:40:36 +0100

    feat : création de classe app_user

--- app/Repositories/authentificationRepository.php
+++ app/Repositories/authentificationRepository.php
@@ -6,7 +6,7 @@ use APP\Models\authentification;
 use config\Database;
 use PDO;
 
-class authentificationRepository 
+class AuthentificationRepository 
 {
     private PDO $pdo;
     public function __construct($pdo){
@@ -34,7 +34,11 @@ class authentificationRepository
             $stmt = $this->pdo->prepare("SELECT * FROM fisherman WHERE id_user = :id");
             $stmt->execute ('id'=>$data['is-user']);
             $dataF = $stmt->fetch(PDO::FETCH_ASSOC);
-            return new fishermen($dataF['id_user'], $dataF['email'], $dataF['password_hash'], $dataF['role']);
+        
+            return new Fishermen(
+                $dataF['id_user'], $dataF['email'], $dataF['password_hash'], 
+                $dataF['club'], $dataF['region'], $dataF['type_peche']
+            );
         }
 
         return new User($data['id_user'], $data['email'], $data['password_hash'], $data['role']);
