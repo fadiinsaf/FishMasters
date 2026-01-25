918b8a91c95ed9499685a4d93e59c6907b20ee2b
Mariam982440 <mariam1elhaimer@gmail.com>
2026-01-25 20:25:14 +0100

    feat : création de la classe session pour la gestion da la connexion

---
+++ app/Models/session.php
@@ -0,0 +1,32 @@
+<?php
+namespace app\Core;
+
+class Session {
+    public function __construct() {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+    }
+
+    public function set(string $key, $value): void {
+        $_SESSION[$key] = $value;
+    }
+
+    public function get(string $key) {
+        return $_SESSION[$key] ?? null;
+    }
+
+    public function has(string $key): bool {
+        return isset($_SESSION[$key]);
+    }
+
+    public function remove(string $key): void {
+        unset($_SESSION[$key]);
+    }
+
+    public function destroy(): void {
+        session_destroy();
+        $_SESSION = [];
+    }
+}
+?>
